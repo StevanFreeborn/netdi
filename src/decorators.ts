7d8b94cf032eb07803fb1a7e73396a0bf5c6a2c8
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-05-12 12:15:20 -0500

    fix: make metadata key's constants

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,15 +1,18 @@
 import type { ServiceIdentifier } from './types.js';
 
+export const DI_PARAM_TYPES = 'di:paramtypes';
+export const DI_INJECTABLE = 'di:injectable';
+
 export function inject<T>(serviceType: ServiceIdentifier<T>): ParameterDecorator {
   // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
   return (target: Object, _: string | symbol | undefined, parameterIndex: number) => {
-    Reflect.defineMetadata('di:paramtypes', serviceType, target, parameterIndex.toString());
+    Reflect.defineMetadata(DI_PARAM_TYPES, serviceType, target, parameterIndex.toString());
   };
 }
 
 export function injectable(): ClassDecorator {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   return (target: Function) => {
-    Reflect.defineMetadata('di:injectable', true, target);
+    Reflect.defineMetadata(DI_INJECTABLE, true, target);
   };
 }
